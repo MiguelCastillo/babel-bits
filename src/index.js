8c16a5ce815bf2268c14ff374a92510e91718981
Miguel Castillo <manchagnu@gmail.com>
2015-06-22 23:36:03 -0400

    refactored the babelize signature to take in an options object rather than reading it from the context

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-/**
- * babel bits module to convert ESnext feature to ES5 equivalents
- */
 var babelCore = require('babel-core');
 var extend = require('xtend');
 
-function babelize(data) {
-  _run(data, this.options);
+function result(value, data) {
+  return typeof value === 'function' ? value(data) : value;
+}
+
+function babelize(data, options) {
+  _run(data, options);
 }
 
 babelize.config = function(options) {
@@ -17,7 +18,7 @@ babelize.config = function(options) {
 function _run(data, options) {
   options = options || {};
   var settings = extend({}, options);
-  settings.filename = (options.filename && options.filename(data)) || data.path;
+  settings.filename = result(options.filename, data) || data.path;
   data.source = babelCore.transform(data.source, settings).code;
 }
 
